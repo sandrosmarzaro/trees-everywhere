396aeec2210e618069b0e88e612de74a00d7975b
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-07-25 23:59:54 -0300

    feat: create serializers to bulk trees

--- apps/trees/serializers.py
+++ apps/trees/serializers.py
@@ -54,3 +54,39 @@ class PlantedTreeSerializer(serializers.ModelSerializer):
         return services.plant_tree(
             user=self.context['request'].user, **validated_data
         )
+
+
+class PlantedTreeItemSerializer(serializers.Serializer):
+    tree = TreeSerializer(write_only=True)
+    latitude = serializers.DecimalField(max_digits=9, decimal_places=6)
+    longitude = serializers.DecimalField(max_digits=9, decimal_places=6)
+
+
+class PlantedTreeListSerializer(serializers.Serializer):
+    plants = PlantedTreeItemSerializer(many=True)
+    account_id = serializers.PrimaryKeyRelatedField(
+        queryset=Account.objects.none(), write_only=True
+    )
+
+    def __init__(self, *args: tuple, **kwargs: dict[str, Any]) -> None:
+        super().__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            self.fields['account_id'].queryset = request.user.accounts.all()
+
+    def create(self, validated_data: dict[str, Any]) -> list[PlantedTree]:
+        account = validated_data['account_id']
+        plants_data = validated_data['plants']
+
+        plants_list = [
+            (
+                plant_data['tree'],
+                (plant_data['latitude'], plant_data['longitude']),
+            )
+            for plant_data in plants_data
+        ]
+
+        user = self.context['request'].user
+        return services.plant_trees(
+            user=user, account=account, plants=plants_list
+        )
