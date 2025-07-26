28b93ca02a02922b46710607744cbf81430aabe3
Sandro Smarzaro <sansmarzaro@gmail.com>
2025-07-26 00:00:18 -0300

    feat: create trees views

--- apps/trees/views.py
+++ apps/trees/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from __future__ import annotations
 
-# Create your views here.
+from django.db.models import QuerySet
+from rest_framework import generics, permissions
+
+from apps.core.permissions import IsOwner
+
+from .models import PlantedTree, Tree
+from .serializers import (
+    PlantedTreeListSerializer,
+    PlantedTreeSerializer,
+    TreeSerializer,
+)
+
+
+class PlantedTreeListByUserAPIView(generics.ListAPIView):
+    serializer_class = PlantedTreeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[PlantedTree]:
+        return PlantedTree.objects.filter(
+            user=self.request.user
+        ).select_related('user', 'tree', 'account')
+
+
+class PlantedTreeAPIView(generics.RetrieveAPIView):
+    serializer_class = PlantedTreeSerializer
+    queryset = PlantedTree.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+
+class PlantedTreeCreateAPIView(generics.CreateAPIView):
+    serializer_class = PlantedTreeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PlantedTreeBulkCreateAPIView(generics.CreateAPIView):
+    serializer_class = PlantedTreeListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PlantedTreeListByAccountsAPIView(generics.ListAPIView):
+    serializer_class = PlantedTreeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self) -> QuerySet[PlantedTree]:
+        user = self.request.user
+        user_accounts = user.accounts.all()
+        return PlantedTree.objects.filter(
+            account__in=user_accounts
+        ).select_related('user', 'tree', 'account')
+
+
+class TreeListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = TreeSerializer
+    queryset = Tree.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TreeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TreeSerializer
+    queryset = Tree.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
